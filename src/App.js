a1eddbde818885de715f3e7f8c9db11790a26fb0
Jed <kohjedrek@yahoo.com.sg>
2024-02-25 00:42:13 +0800

    Removed redundant codes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ function App() {
   return (
     <div className="App">
       <MainDisplay />
-      {/* Title Class is from index.css styles
-      <div className="title">Money Go Where</div>
-
-      This are the tailWind Styles without a class (Inline Styles)
-      <div className="text-teal-600 text-4xl text-center">WELCOME 🌭</div>
-
-      {Do not code here, put your components Home for reference. App is only for rendering components. Will delete the above} */}
     </div>
   );
 }
