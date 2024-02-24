5e57808ea3f26d9c88b5c3063510d2686f32ce08
Jed <kohjedrek@yahoo.com.sg>
2024-02-25 00:41:22 +0800

    Updated styles

--- src/components/pages/shared/contentArea.jsx
+++ src/components/pages/shared/contentArea.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function contentArea() {
-  return <div className="Content-area">contentArea</div>;
+  return (
+    <div className="Content-area">
+      <div>Title</div>
+      <div>Content</div>
+    </div>
+  );
 }
 
 export default contentArea;
