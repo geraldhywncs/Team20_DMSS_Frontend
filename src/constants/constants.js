152cc228806f271bf615378e3da0ff54c623c95c
Jed <kohjedrek@yahoo.com.sg>
2024-02-24 18:22:51 +0800

    Added constant file to store variables/strings

---
+++ src/constants/constants.js
@@ -0,0 +1,10 @@
+export const API_URL = "https://api.example.com";
+export const APP_TITLE = "MoneyGoWhere";
+
+// Temp value pairs
+export const TEMP_USER = "Steven Lim 帅哥";
+
+export const ERROR_MESSAGES = {
+  NETWORK_ERROR: "Network error. Please try again later.",
+  AUTH_ERROR: "Authentication failed. Please log in again.",
+};
