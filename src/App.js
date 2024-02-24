a9ee542a236ec23c74298a4e9cdb7d304dfd14f3
Jed <kohjedrek@yahoo.com.sg>
2024-02-24 18:23:20 +0800

    Reorganise app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./styles/index.css";
-import HomeSample from "./components/pages/home/home";
+import MainDisplay from "./components/pages/mainDisplay/mainDisplay";
 
 function App() {
   return (
     <div className="App">
-      {/* Title Class is from index.css styles */}
+      <MainDisplay />
+      {/* Title Class is from index.css styles
       <div className="title">Money Go Where</div>
 
-      {/* This are the tailWind Styles without a class (Inline Styles) */}
+      This are the tailWind Styles without a class (Inline Styles)
       <div className="text-teal-600 text-4xl text-center">WELCOME 🌭</div>
 
-      {/* {Do not code here, put your components HomeSample for reference. App is only for rendering components. Will delete the above} */}
-      <HomeSample />
+      {Do not code here, put your components Home for reference. App is only for rendering components. Will delete the above} */}
     </div>
   );
 }
