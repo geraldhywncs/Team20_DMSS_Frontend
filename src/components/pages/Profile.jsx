f29b0ba01e2c0553d3a91d41cabbdad22b67ff08
geraldhywncs <gerald.hoo@ncs.com.sg>
2024-02-25 21:16:43 +0800

    add MyProfile and Friends in profile page

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import "../../styles/pages/Profile.css";
 import Section from "./shared/Section";
+import Friends from "./shared/Friends";
 
 function Profile() {
+  const friends = [
+    { name: "Jun Jie", username: "junjie", isFriend: true },
+    { name: "Wei Jie", username: "weijie", isFriend: true },
+    { name: "Jedrek", username: "jedrek", isFriend: true },
+    { name: "Weii Zee", username: "weiizee", isFriend: false },
+  ];
   return (
     <>
       <MyProfile />
-      <Friends />
+      <Section headerName={"Friends"}>
+        <Friends friends={friends} />
+      </Section>
       <Groups />
     </>
   );
 }
 
 function MyProfile() {
+  const name = "Gerald";
+  const username = "geraldhyw";
+  const bio = `Pastry sprinkles marzipan tiramisu ipsum marzipan. Cream ipsum tiramisu croissant cake tiramisu. Tiramisu orange croissant apple sweet cream. Biscuit marzipan tiramisu dolor croissant sprinkles.`;
   return (
     <Section headerName={"My Profile"}>
-      <div>test</div>
-    </Section>
-  );
-}
-
-function Friends() {
-  return (
-    <Section headerName={"Friends"}>
-      <div>test</div>
+      <div className="profile-info-container">
+        <div className="profile-info body-medium font-regular">{`${name} | @${username}`}</div>
+        <div className="profile-info body-medium font-regular">{bio}</div>
+      </div>
     </Section>
   );
 }
---
+++ src/components/pages/shared/Friends.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Friends(props) {
+  const { friends } = props;
+  return (
+    <div className="friends-container">
+      {friends.map((friend) => (
+        <FriendCard
+          name={friend.name}
+          username={friend.username}
+          isFriend={friend.isFriend}
+        />
+      ))}
+    </div>
+  );
+}
+
+function FriendCard(props) {
+  const { name, username, isFriend } = props;
+  return (
+    <div className="friend-container">
+      <div className="friend-info body-large font-medium">{`${name} | @${username}`}</div>
+      <span className="material-icons">
+        {isFriend ? "person_remove" : "person_add"}
+      </span>
+    </div>
+  );
+}
+
+export default Friends;
