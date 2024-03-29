cbe3d2f6438888d3ac54400c754dc18b78ff09d2
JedrekNCS <wildrekk@gmail.com>
2024-03-29 15:14:31 +0800

    Relocated dashboard components dir

--- src/components/shared/dashboard/GraphDayLayout.jsx
+++ src/components/shared/dashboard/GraphDayLayout.jsx
@@ -10,4 +10,17 @@ function GraphDayLayout() {
   );
 }
 
+
+
+
+
 export default GraphDayLayout;
+
+/*
+1. Needs to call the expense API based on the expense. Select * from expenses based on user id.
+2. Once the expense is selected, get the receipt_id. 
+3. Select * from receipt_id and get the info
+
+From there use the necessary filters to filter out the values.
+
+*/
