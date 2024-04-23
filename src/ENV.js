82d261b28e93e5a40c570745603dbe070d608fbc
Wee Jun Jie <d1357846@ncsdevnet.com>
2024-04-23 14:07:57 +0800

    change env.js

--- src/ENV.js
+++ src/ENV.js
@@ -9,9 +9,9 @@ function isHostedOnAWS() {
 if (isHostedOnAWS()) {
   apiHost = "http://3.26.203.126:5000";
 } else {
-  // apiHost = "http://13.239.22.94:5000";
+  apiHost = "http://13.239.22.94:5000";
   //apiHost = "http://172.20.0.1:5000";
-  apiHost = "http://127.0.0.1:5000";
+  // apiHost = "http://127.0.0.1:5000";
 }
 
 export const REACT_APP_apiHost = apiHost;
